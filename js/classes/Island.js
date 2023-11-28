b3bd0b58432aba73ef844e8a590f0afbf33630b9
Clueless <91685310+BrendVandenEynde@users.noreply.github.com>
2023-11-29 00:01:58 +0100

    Add color property to island, update save/load
    Include the color property when saving and loading islands in the World class. 
    Modify the Island class to properly handle the color property. This ensures that the color is correctly saved and loaded when interacting with the islands.

--- js/classes/Island.js
+++ js/classes/Island.js
@@ -1,46 +1,49 @@
 export default class Island {
   constructor(name) {
-    this.name = name || this.getRandomName(); // this.name is het nieuwe / huidige eiland dat er wordt gespawnd, de naam krijgt het van getRandomName functie
-    this.color = this.getRandomColor(); // Het nieuwe eiland dat aangemaakt wordt krijgt tezamen met de naam een willekeurige kleur dankzij de gerRandomColor function.
+    this.name = name || this.getRandomName();
+    this.color = this.getRandomColor();
   }
-    getRandomColor() {
-      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    }
-  
-    remove() {
-      // JS animations api, fade out
-      // remove the element when the animation ended
-    }
-  
-    getRandomName() {
-      // array with 10 random island names
-      const names = [
-        "Palmtree beach",
-        "Sandy beach",
-        "Tropical beach",
-        "Palm beach",
-        "Sunny beach",
-        "Paradise beach",
-        "Sunny island",
-        "Tropical island",
-        "Palm island",
-        "Paradise island",
-        "Coral Cove",
-        "Azure Oasis",
-        "Tropicana Haven",
-        "Serene Shores",
-        "Mango Mirage",
-        "Golden Lagoon",
-        "Isle of Tranquility",
-        "Emerald Eden",
-        "Sunset Sanctuary",
-        "Coconut Bliss Cove"
-      ];
-  
-      // // return a random name from the array
-      const randomIndex = Math.floor(Math.random() * names.length); // Generate a random index within the range of the 'names' array
-      return names[randomIndex]; // Return the name at the randomly generated index
 
-    }
+  getRandomColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
-  
+
+  toJSON() {
+    return {
+      name: this.name,
+      color: this.color,
+    };
+  }
+
+  remove() {
+    // JS animations api, fade out
+    // remove the element when the animation ended
+  }
+
+  getRandomName() {
+    const names = [
+      "Palmtree beach",
+      "Sandy beach",
+      "Tropical beach",
+      "Palm beach",
+      "Sunny beach",
+      "Paradise beach",
+      "Sunny island",
+      "Tropical island",
+      "Palm island",
+      "Paradise island",
+      "Coral Cove",
+      "Azure Oasis",
+      "Tropicana Haven",
+      "Serene Shores",
+      "Mango Mirage",
+      "Golden Lagoon",
+      "Isle of Tranquility",
+      "Emerald Eden",
+      "Sunset Sanctuary",
+      "Coconut Bliss Cove"
+    ];
+    const randomIndex = Math.floor(Math.random() * names.length);
+    return names[randomIndex];
+  }
+}
--- js/classes/World.js
+++ js/classes/World.js
@@ -1,119 +1,121 @@
-import Island from './Island.js'; // Import the Island class
+import Island from './Island.js';
 
 export default class World {
-    constructor() {
-      this.islands = []; // a good place to keep track of your islands
-      this.hookEvents(); // let's kick things off by hooking up events
-    }
-  
-    hookEvents() {
-      const btnAddIsland = document.getElementById('btnAddIsland');
-      btnAddIsland.addEventListener('click', () => this.addIslandOnce());
-  
-      const btnSave = document.getElementById('btnSave');
-      btnSave.addEventListener('click', () => this.save());
-  
-      const btnLoad = document.getElementById('btnLoad');
-      btnLoad.addEventListener('click', () => this.load());
-    }
-  
-    addIslandOnce() {
-      const btnAddIsland = document.getElementById('btnAddIsland');
-      btnAddIsland.removeEventListener('click', () => this.addIslandOnce());
-  
-      this.addIsland();
-  
-      btnAddIsland.addEventListener('click', () => this.addIslandOnce());
-    }
-  
-    save() {
-      const savedIslands = this.islands.map(island => ({
-        name: island.name,
-        position: {
-          x: island.islandElement.offsetLeft,
-          y: island.islandElement.offsetTop,
-        },
-      }));
-  
-      const savedIslandsJSON = JSON.stringify(savedIslands);
-      localStorage.setItem('savedIslands', savedIslandsJSON);
-  
-      console.log('Islands saved!');
-    }
-  
-    load() {
-      const savedIslandsJSON = localStorage.getItem('savedIslands');
-      if (savedIslandsJSON) {
-        const savedIslands = JSON.parse(savedIslandsJSON);
-        savedIslands.forEach(savedIsland => {
-          const island = new Island(savedIsland.name);
-          this.islands.push(island);
-  
-          const islandElement = document.createElement('div');
-          islandElement.classList.add('island');
-          document.body.appendChild(islandElement);
-  
-          islandElement.style.left = savedIsland.position.x + 'px';
-          islandElement.style.top = savedIsland.position.y + 'px';
-          islandElement.style.transform = `translate(${savedIsland.position.x}px, ${savedIsland.position.y}px)`;
-          islandElement.style.backgroundColor = island.getRandomColor();
-          islandElement.innerHTML = savedIsland.name;
-  
-          islandElement.addEventListener('click', () => {
-            this.removeIsland(island, islandElement);
-          });
+  constructor() {
+    this.islands = [];
+    this.hookEvents();
+  }
+
+  hookEvents() {
+    const btnAddIsland = document.getElementById('btnAddIsland');
+    btnAddIsland.addEventListener('click', () => this.addIslandOnce());
+
+    const btnSave = document.getElementById('btnSave');
+    btnSave.addEventListener('click', () => this.save());
+
+    const btnLoad = document.getElementById('btnLoad');
+    btnLoad.addEventListener('click', () => this.load());
+  }
+
+  addIslandOnce() {
+    const btnAddIsland = document.getElementById('btnAddIsland');
+    btnAddIsland.removeEventListener('click', () => this.addIslandOnce());
+
+    this.addIsland();
+
+    btnAddIsland.addEventListener('click', () => this.addIslandOnce());
+  }
+
+  save() {
+    const savedIslands = this.islands.map(island => ({
+      name: island.name,
+      color: island.color, // Include the color property
+      position: {
+        x: island.islandElement.offsetLeft,
+        y: island.islandElement.offsetTop,
+      },
+    }));
+
+    const savedIslandsJSON = JSON.stringify(savedIslands);
+    localStorage.setItem('savedIslands', savedIslandsJSON);
+
+    console.log('Islands saved!');
+  }
+
+  load() {
+    const savedIslandsJSON = localStorage.getItem('savedIslands');
+    if (savedIslandsJSON) {
+      const savedIslands = JSON.parse(savedIslandsJSON);
+      savedIslands.forEach(savedIsland => {
+        const island = new Island(savedIsland.name);
+        island.color = savedIsland.color; // Set the color for the island
+        this.islands.push(island);
+
+        const islandElement = document.createElement('div');
+        islandElement.classList.add('island');
+        document.body.appendChild(islandElement);
+
+        islandElement.style.left = savedIsland.position.x + 'px';
+        islandElement.style.top = savedIsland.position.y + 'px';
+        islandElement.style.transform = `translate(${savedIsland.position.x}px, ${savedIsland.position.y}px)`;
+        islandElement.style.backgroundColor = island.color; // Set the saved color
+        islandElement.innerHTML = savedIsland.name;
+
+        islandElement.addEventListener('click', () => {
+          this.removeIsland(island, islandElement);
         });
-  
-        console.log('Islands loaded!');
-      } else {
-        console.log('No saved islands found.');
-      }
-    }
-  
-    getCoordinates() {
-      let randomSign = Math.random() < 0.5 ? -1 : 1;
-      return {
-        x: ((Math.random() * window.innerWidth) / 2) * randomSign,
-        y: ((Math.random() * window.innerHeight) / 2) * randomSign,
-      };
-    }
-  
-    addIsland() {
-      const island = new Island();
-      this.islands.push(island);
-  
-      const islandElement = document.createElement('div');
-      islandElement.classList.add('island');
-      document.body.appendChild(islandElement);
-  
-      const coordinates = this.getCoordinates();
-      islandElement.style.left = coordinates.x + 'px';
-      islandElement.style.top = coordinates.y + 'px';
-      islandElement.style.transform = `translate(${coordinates.x}px, ${coordinates.y}px)`;
-      islandElement.style.backgroundColor = island.getRandomColor();
-      islandElement.innerHTML = island.getRandomName();
-  
-      islandElement.addEventListener('click', () => {
-        this.removeIsland(island, islandElement);
       });
-  
-      island.islandElement = islandElement;
-  
-      console.log('Island added!');
-    }
-  
-    removeIsland(island, islandElement) {
-      const index = this.islands.indexOf(island);
-      if (index !== -1) {
-        this.islands.splice(index, 1);
-      }
-  
-      document.body.removeChild(islandElement);
-  
-      console.log('Island removed!');
-    }
-  
-    moveIsland(island) {
-      // Placeholder for animation logic
+
+      console.log('Islands loaded!');
+    } else {
+      console.log('No saved islands found.');
     }
   }
+
+  getCoordinates() {
+    let randomSign = Math.random() < 0.5 ? -1 : 1;
+    return {
+      x: ((Math.random() * window.innerWidth) / 2) * randomSign,
+      y: ((Math.random() * window.innerHeight) / 2) * randomSign,
+    };
+  }
+
+  addIsland() {
+    const island = new Island();
+    this.islands.push(island);
+
+    const islandElement = document.createElement('div');
+    islandElement.classList.add('island');
+    document.body.appendChild(islandElement);
+
+    const coordinates = this.getCoordinates();
+    islandElement.style.left = coordinates.x + 'px';
+    islandElement.style.top = coordinates.y + 'px';
+    islandElement.style.transform = `translate(${coordinates.x}px, ${coordinates.y}px)`;
+    islandElement.style.backgroundColor = island.color;
+    islandElement.innerHTML = island.getRandomName();
+
+    islandElement.addEventListener('click', () => {
+      this.removeIsland(island, islandElement);
+    });
+
+    island.islandElement = islandElement;
+
+    console.log('Island added!');
+  }
+
+  removeIsland(island, islandElement) {
+    const index = this.islands.indexOf(island);
+    if (index !== -1) {
+      this.islands.splice(index, 1);
+    }
+
+    document.body.removeChild(islandElement);
+
+    console.log('Island removed!');
+  }
+
+  moveIsland(island) {
+    // Placeholder for animation logic
+  }
+}
